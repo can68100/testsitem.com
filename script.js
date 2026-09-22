bb21c95fe052639440341b0351a5b7b1eca363f7
can68100 <cankanada271@gmail.com>
2026-09-22 17:41:47 +0300

    script.js

--- script.js
+++ script.js
@@ -42,7 +42,18 @@ const MOVIES_DATA = [
         rating: "8.0",
         image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcToosLXR0KYVkK75yIlaupS1TzggERDa51ZBNBNhI6b_Q&s=10",
         trailer: <iframe width="560" height="315" src="https://www.youtube.com/embed/__gOZ6gJT8c?si=fUkerjTRJH_vbVaR" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-        watch_url: "https://dn790001.ca.archive.org/0/items/zubuk_1980/Z%C3%BCb%C3%BCk.mp4",
+        watch_url: "    {
+        id: 9,
+        title: "zübük ",
+        genre: "komedi",
+        year: "2019",
+        rating: "8.0",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcToosLXR0KYVkK75yIlaupS1TzggERDa51ZBNBNhI6b_Q&s=10",
+        trailer: <iframe width="560" height="315" src="https://www.youtube.com/embed/__gOZ6gJT8c?si=fUkerjTRJH_vbVaR" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        watch_url: "<iframe src="https://dn790001.ca.archive.org/0/items/zubuk_1980/Z%C3%BCb%C3%BCk.mp4" width="100%" height="450" frameborder="0" allowfullscreen></iframe>",
+        desc: "  Aziz Nesin’in aynı adlı eserinden uyarlanan Zübük, çıkarcı, ikiyüzlü ve halkı kandırarak yükselen bir siyasetçi olan İbrahim Zübükzade'nin rüşvet, yalan ve düzenbazlıkla nasıl merdivenleri tırmandığını anlatan bir siyasi mizah hikayesidir."
+    },
+",
         desc: "  Aziz Nesin’in aynı adlı eserinden uyarlanan Zübük, çıkarcı, ikiyüzlü ve halkı kandırarak yükselen bir siyasetçi olan İbrahim Zübükzade'nin rüşvet, yalan ve düzenbazlıkla nasıl merdivenleri tırmandığını anlatan bir siyasi mizah hikayesidir."
     },
 
